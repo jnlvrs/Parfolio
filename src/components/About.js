4d55c147c6057662bf0f554eef355132304e8d3f
jnlvrs <71854392+jnlvrs@users.noreply.github.com>
2022-07-07 12:30:51 -0500

    added photos to fill space in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,32 @@
 import React from "react";
-import HeadShot from "../img/HeadShot.jpg";
+import HeadShot from "../img/HeadShot2.jpg";
+import Wife from "../img/Wife.jpg";
+import Kiddos from "../img/Kiddos.jpg";
 
 function About() {
   return (
-    <div id="About" className="about-container bg3g borderb">
+    <div id="About" className="about-container bg2 borderb">
       <div className="about-heading-container">
         <h2>About Me</h2>
         <div className="about-p-img-container">
           <div className="about-img-container">
-            <img src={HeadShot} className="about-img" />
+            <img src={HeadShot} className="octo img-me " />
+            <img src={Wife} className="octo img-wife" />
+            <img src={Kiddos} className="octo img-kiddos" />
           </div>
           <div className="about-p-container bg1 hl2 bxs">
             <p>
-              &emsp;&emsp;Hi, am a JavaScript Developer located in Minnesota. I
-              am learning and growing a passion for coding thanks to my cousins.
-              I am a well-organized individual that enjoys solving problems and
+              &emsp;Hi, am a JavaScript Developer located in Minnesota. I am
+              learning and growing a passion for coding thanks to my cousins. I
+              am a well-organized individual that enjoys solving problems and
               has a high drive and interest to move forward in my life as well
               as my career.
             </p>
             <br />
             <p>
               &emsp;&emsp;I am a father of three kiddos that I very much love.
-              Interested in the entire full stack spectrum and working on projects
-              with positive people!
+              Interested in the entire Full-Stack spectrum and working on
+              projects with positive people!
             </p>
           </div>
         </div>
