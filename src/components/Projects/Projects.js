219c920ea40fa2adda63e83129d82d9061ef3db4
jnlvrs <71854392+jnlvrs@users.noreply.github.com>
2022-09-06 14:19:05 -0500

    more css

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,19 +5,8 @@ function Projects() {
   return (
     <div id="Projects" className="projects bg2 borderb">
       <h2 className="projects-h2">My Projects</h2>
-      <div className="card-container">
-        {ProjectData.map((p, i) => (
-        <div className="card" key={i}>
-          <h3 className="title">{p.title}</h3>
-          <div className="bar">
-            <div className="emptybar"></div>
-            <div className="filledbar"></div>
-          </div>
-          <div className="project-title">
-            <a href={p.link}>Github repository</a>
-          </div>
-        </div>
-        ))}
+      <div className="carousels-container">
+        
       </div>
     </div>
   );
